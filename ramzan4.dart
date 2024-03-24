153ba0994d473d2a55930511e22ac1dd563bebea
MuhammadZubair786/ <mzhassan444@gmail.com>
2024-03-24 17:26:36 +0500

    update

---
+++ ramzan4.dart
@@ -0,0 +1,105 @@
+import 'dart:ffi';
+import 'dart:io';
+
+void main() {
+  // stdout.writeln("Enter number");
+  // int a = int.parse(stdin.readLineSync()!);
+  // if(a%2==0){
+  //   print("${a} is Even Number");
+  // }
+  // else{
+  //   print("${a} is odd Number");
+  // }
+
+  // stdout.write("Enter Your Name");
+  // String name = stdin.readLineSync()!;
+
+  // stdout.write("eNTER YOUR AGE");
+  // int age = int.parse(stdin.readLineSync()!);
+
+  // final stopwatch = Stopwatch()..start();
+
+  // if (name[0].toLowerCase() == "a" ||
+  //     name[0].toLowerCase() == "e" ||
+  //     name[0].toLowerCase() == "i" ||
+  //     name[0].toLowerCase() == "o" ||
+  //     name.toLowerCase() == "u") {
+  //       if(age>=18){
+  //         print("You eligible for Flutter course");
+
+  //       }
+  //       else{
+  //         print("your age is not more then or equal to 18");
+  //       }
+    
+  // }
+  // else{
+  //   print("ap ka name ma koi vowel nhi ha ");
+  // }
+
+  // print(stopwatch.elapsed);
+
+  // km 
+  // 1=>km =>10
+  // hyderabad=>140km=>140*10=>1400
+  // nooribad=>70km=>70*10=>700
+  // String a = "50";
+  // int a =10;
+
+  // int day =4;
+  // switch(day){
+  //   case 0 :
+  //   print("Today is Sunday");
+  //   case 1 :
+  //   print("Today is Monday");   
+  //    case 2 :
+  //   print("Today is Tuesday");   
+  //    case 4 :
+  //   print("Today is wednesday"); 
+  //   case 4 :
+  //   print("Today is thursday");    
+
+  //   case 5 :
+  //   print("Today is friday");    
+  //   case 4 :
+  //   print("Today is saturday");
+  //   default:
+  //   print("encorrect value");
+  // }
+
+  // switch(a){
+  //   case 0 :
+  //   print("value is 0");
+  //   case 1:
+  //   print("value is 1");
+  //   case 2:
+  //   print("value is 2");
+  //   case 5:
+  //   print("value is 5");
+  //   default:
+  //   print("new value ");
+
+  // }
+
+  // String a = "+";
+  // int num1 =4;
+  // int num2 = 5;
+
+  // switch(a){
+  //   case "+":
+  //   print(num1+num2);
+  //   print("add");
+  //    case "-":
+  //     print(num1-num2);
+  //   print("sub");
+  //   default:
+  //   print("encorrect value");
+
+  // }
+
+  // List age =[10,20,30,"test"];
+  // print(age);
+  // print(age.runtimeType);
+
+
+}
