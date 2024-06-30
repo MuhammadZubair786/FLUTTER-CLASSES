87bfba3ed5b3a6ec95bca9c8954a19358f16ab75
rehanmemon007 <rehanmemon1920@yahoo.com>
2024-06-30 20:13:57 +0500

    update

--- lib/Components/Auth/SignUp.dart
+++ lib/Components/Auth/SignUp.dart
@@ -1,8 +1,14 @@
-// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables, avoid_print, use_build_context_synchronously
+
+import 'dart:async';
+import 'dart:io';
 
 import 'package:ecomapp/Components/Auth/Login.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_database/firebase_database.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
 class SignUpScreen extends StatefulWidget {
   const SignUpScreen({super.key});
@@ -17,29 +23,134 @@ class _SignUpScreenState extends State<SignUpScreen> {
   TextEditingController password = TextEditingController();
 
   GlobalKey<FormState> formState = GlobalKey<FormState>();
+
+  var _image;
+  var filedata;
+  var uid;
+  var imagelink;
+
+  final ImagePicker _picker = ImagePicker();
+
   var regpassword =
       RegExp('^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#\$%&\*])(?=.{8,})');
 
   Future<void> RegsisterUser() async {
     print(email.text);
     print(password.text);
-    try{
-       final userCredential = await FirebaseAuth.instance
-        .createUserWithEmailAndPassword(email: email.text, password: password.text);
-    final user = userCredential.user;
-    print(user?.uid);
-    email.text="";
-    password.text="";
-    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
-
-
-    }
-    catch(e){
+    try {
+      final userCredential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(
+              email: email.text, password: password.text);
+      final user = userCredential.user;
+      print(user?.uid);
+      
+      setState(() {
+        uid= user?.uid;
+      });
+      // email.text = "";
+      // password.text = "";
+      imageStoreStorage();
+     
+    } catch (e) {
       print(e.toString());
-
     }
+  }
 
-   
+  pickImage(source) async {
+    final pickedFile = await _picker.pickImage(source: source);
+
+    print(pickedFile!.path);
+    setState(() {
+      _image = File(pickedFile!.path);
+      filedata = pickedFile;
+    });
+    // imageStoreStorage();
+  }
+
+  imageStoreStorage() async {
+    try {
+      FirebaseStorage storage = FirebaseStorage.instance;
+      Reference storageRef = storage.ref().child("staff/${filedata.path}");
+      UploadTask upload = storageRef.putFile(_image);
+      TaskSnapshot snapshot = await upload.whenComplete(() => ());
+      String downloadUrl = await snapshot.ref.getDownloadURL();
+      print(downloadUrl);
+      setState(() {
+      imagelink=downloadUrl;
+        
+      });
+      storedb();
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  storedb() async {
+    var dbinstance = FirebaseDatabase.instance;
+    DatabaseReference dbref = dbinstance.reference();
+
+    var userObj = {
+      "email":email.text.toString(),
+      "password":password.text.toString(),
+      "imageUrl":imagelink,
+      "name":"test",
+      "age":20
+    };
+    
+    
+  await  dbref.child("user").child(uid).push().update(userObj);
+     Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => LoginScreen()));
+
+  }
+
+  showBottomSheet() {
+    showModalBottomSheet<void>(
+      // context and builder are
+      // required properties in this widget
+      context: context,
+      builder: (BuildContext context) {
+        // we set up a container inside which
+        // we create center column and display text
+
+        // Returning SizedBox instead of a Container
+        return SizedBox(
+          height: 50,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      // Navigator.pop(context);
+
+                      pickImage(ImageSource.camera);
+                    },
+                    child: CircleAvatar(
+                      child: Icon(Icons.camera),
+                    ),
+                  ),
+                  SizedBox(
+                    width: 30,
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.pop(context);
+                      pickImage(ImageSource.gallery);
+                    },
+                    child: CircleAvatar(
+                      child: Icon(Icons.photo),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 
   @override
@@ -54,6 +165,17 @@ class _SignUpScreenState extends State<SignUpScreen> {
                 height: 30,
               ),
               Text("User Sign up"),
+              GestureDetector(
+                  onTap: () {
+                    showBottomSheet();
+                  },
+                  child: _image == null
+                      ? CircleAvatar(
+                          radius: 80,
+                          backgroundImage: NetworkImage(
+                              "https://img.freepik.com/premium-vector/energy-flow-qi-chee-icon-vector-image-can-be-used-alternative-medicine_120816-199403.jpg?uid=R76992221&ga=GA1.1.1881161492.1714914526&semt=ais_user"))
+                      : CircleAvatar(
+                          radius: 80, backgroundImage: FileImage(_image))),
               Container(
                 margin: EdgeInsets.all(10),
                 child: TextFormField(
@@ -160,11 +282,12 @@ class _SignUpScreenState extends State<SignUpScreen> {
                     // }
                   },
                   child: Text("Submit")),
-                    ElevatedButton(
+              ElevatedButton(
                   onPressed: () {
                     // if (formState.currentState!.validate()) {
                     // } else {
-                    Navigator.push(context, MaterialPageRoute(builder: (context)=>LoginScreen()));
+                    Navigator.push(context,
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
                     // }
                   },
                   child: Text("Already Account,Login ")),
